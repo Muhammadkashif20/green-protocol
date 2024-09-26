8e4195de2a6298e8d9921ec0c940f1f96a469051
muhammadkashif20 <kashif28@gmail.com>
2024-09-26 22:50:53 +0500

    Solution Section Has Been Completed Few Working to Ready

--- src/Components/Solution.jsx
+++ src/Components/Solution.jsx
@@ -1,93 +1,29 @@
 import React from 'react'
-
+import image1 from '../Images/Solution1.png' 
 const Solution = () => {
   return (
-    <section className="Solution text-gray-600 body-font w-50rem">
-       <h1 className='text-green-600 font-bold text-5xl ms-[8rem] mt-2  ' >Solution</h1>
+    <div className='Solution'>
+       <h1 className='text-green-600 font-bold text-5xl ms-[8rem] mt-2 ' >Solution</h1>
          <div className='bg-green-400 ms-[8rem] w-[60rem] h-1 mt-4 rounded-md'></div>
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/420x260"
-          />
-        </a>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/421x261"
-          />
-        </a>
-       
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/422x262"
-          />
-        </a>
-       
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/423x263"
-          />
-        </a>
-        
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/424x264"
-          />
-        </a>
-        
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/425x265"
-          />
-        </a>
-        
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/427x267"
-          />
-        </a>
-        
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a className="block relative h-48 rounded overflow-hidden">
-          <img
-            alt="ecommerce"
-            className="object-cover object-center w-full h-full block"
-            src="https://dummyimage.com/428x268"
-          />
-        </a>
-      </div>
+         <div className='flex flex-row ms-[9rem] gap-0 mt-12'>
+         <div className='card bg-green-600 w-[14.5rem] h-[19rem] flex align-middle justify-center pt-2 flex-col'>
+          <img src={image1} className='h-[10rem] w-[10rem] ms-[2.2rem] my-3'/>
+            <h1 className='text-center text-white text-2xl font-bold'>Green <br/>Energy</h1>
+
+         </div>
+         <div className='card bg-green-500 w-[14.5rem] h-[19rem]'></div>
+         <div className='card bg-green-600 w-[14.5rem] h-[19rem]'></div>
+         <div className='card bg-green-500 w-[14.5rem] h-[19rem]'></div>
+         </div>
+         <div className='flex flex-row ms-[9rem] gap-0'>
+         <div className='card bg-green-500 w-[14.5rem] h-[19rem]'></div>
+         <div className='card bg-green-600 w-[14.5rem] h-[19rem]'></div>
+         <div className='card bg-green-500 w-[14.5rem] h-[19rem]'></div>
+         <div className='card bg-green-600 w-[14.5rem] h-[19rem]'></div>
+         </div>
+         
     </div>
-  </div>
-</section>
+ 
 
   )
 }
